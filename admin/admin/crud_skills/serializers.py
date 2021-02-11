61bc45081fce7afa526b09390f419c6c49bda44c
Quesia <quesiasts@gmail.com>
2021-02-11 15:08:12 -0300

    criaçao das serializers

---
+++ admin/admin/crud_skills/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Skills, Person
+
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skills
+        fields = '__all__'
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__'
